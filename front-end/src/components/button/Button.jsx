e615b966f17d58f8c73eeae76fa12cacc258263d
Ioana Berdei <andreea.ioanaa98@gmail.com>
2025-01-07 17:39:26 +0200

    add logo and partially update color scheme

--- front-end/src/components/button/Button.jsx
+++ front-end/src/components/button/Button.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router"
 
 const Button = (props) => {
     return (
-        <button type={!!props.type ? props.type : "button"} className="block w-64 text-white bg-black rounded-lg hover:bg-white hover:text-black transition-colors duration-200 shadow-md border border-black disabled:opacity-50 disabled:cursor-not-allowed"
+        <button type={!!props.type ? props.type : "button"} className="block w-64 text-white bg-watchme-blue rounded-lg hover:bg-white hover:text-watchme-blue transition-colors duration-200 shadow-md border border-watchme-blue disabled:opacity-50 disabled:cursor-not-allowed"
             onClick={props.onClick} disabled={props.disabled}>
             {!!props.href
                 ? <Link to={props.href} className="block w-full h-full px-6 py-3">
--- front-end/src/components/dashboard/Dashboard.jsx
+++ front-end/src/components/dashboard/Dashboard.jsx
@@ -1,10 +1,11 @@
 import Button from "../button/Button"
+import logoWatchme from "../../assets/logo-watchme.png";
 
 const Dashboard = () => {
     return (
         <div className="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/3">
             <div className="flex flex-col items-center justify-center">
-                <h1 className="text-4xl font-bold text-gray-800 mb-8 select-none">watchme</h1>
+                <img className="mb-8 w-80" src={logoWatchme} alt="logo watchme"></img>
                 <div className="space-y-4">
                     <Button href="./share/" text="share your screen" />
                     <Button href="./view/" text="view someone's screen" />
